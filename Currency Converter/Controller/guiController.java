bceeb5df658960df2575a783376325d19bb7ecc7
Bryan Hor Jin Hao <2702bryan@gmail.com>
2022-05-10 10:49:57 +0800

    Completed the GUI Controller

--- Currency Converter/Controller/guiController.java
+++ Currency Converter/Controller/guiController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -13,7 +14,7 @@ import javafx.scene.control.TextField;
 
 public class guiController implements Initializable{
     @FXML
-    private Label exchageRateLabel, convertedAmountLabel;
+    private Label exchangeRateLabel, convertedAmountLabel;
 
     @FXML
     private Button convertButton, clearButton;
@@ -23,22 +24,39 @@ public class guiController implements Initializable{
 
     @FXML
     private ComboBox currencySelector;
+
+    // private String DEFAULT_CURRENCY = "MYR";
+    private enum Currency {USD, EUR, GBP};
     
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        
+        currencySelector.getItems().addAll(Currency.values());
     }
 
     public void showExchangeRate() {
-        
+        Currency chosenCurrency = (Currency) currencySelector.getValue();
+        double exchangeRate = 0;
+
+        switch (chosenCurrency) {
+            case USD: exchangeRate = 0.23; exchangeRateLabel.setText(Double.toString(exchangeRate)); break;
+            case EUR: exchangeRate = 0.22; exchangeRateLabel.setText(Double.toString(exchangeRate)); break;
+            case GBP: exchangeRate = 0.18; exchangeRateLabel.setText(Double.toString(exchangeRate)); break;
+            default: exchangeRate = 1.00; exchangeRateLabel.setText(Double.toString(exchangeRate));
+        }
     }
     
     public void convert(ActionEvent event) {
-
+        Double amount = Double.parseDouble(baseAmount.getText());
+        Double exchangeRate = Double.parseDouble(exchangeRateLabel.getText());
+        Double convertedAmount = amount * exchangeRate;
+        DecimalFormat df = new DecimalFormat("#.##");
+        convertedAmountLabel.setText(df.format(convertedAmount));
     }
 
     public void clear(ActionEvent event) {
-
+        convertedAmountLabel.setText("");
+        exchangeRateLabel.setText("");
+        baseAmount.clear();
     }
 
 }
